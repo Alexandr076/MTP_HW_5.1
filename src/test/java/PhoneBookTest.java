70fa014a04f233d879ecaa9d691e0b054ee028a0
U_M0ZUN <alexandr0751@yandex.ru>
2023-07-01 22:52:15 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "name1";
+        String number = "123";
+        int result = phoneBook.add(name, number);
+        assertTrue("Неверный результат после добавления номера: " + result, result == 1);
+        name = "name2";
+        number = "1234";
+        result = phoneBook.add(name, number);
+        assertTrue("Неверный результат после добавления второго номера: " + result, result == 2);
+        name = "name2";
+        number = "12345";
+        result = phoneBook.add(name, number);
+        assertTrue("Неверный результат после добавления третьего номера: " + result, result == 2);
+    }
 }
