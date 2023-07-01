e1b266f13745115d282c0b4a7afe45621d5d22b9
U_M0ZUN <alexandr0751@yandex.ru>
2023-07-01 22:53:16 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+
 public class PhoneBook {
+    private int countOfContacts = 0;
+    private HashMap<String, String> contacts = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (contacts.get(name) == null) {
+            contacts.put(name, number);
+            countOfContacts=countOfContacts+1;
+        }
+        return countOfContacts;
     }
 }
